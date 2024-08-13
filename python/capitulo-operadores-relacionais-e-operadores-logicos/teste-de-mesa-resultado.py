34f556c1e07bf63ecb708fbbf6183ee0c72eecc9
Rafael Silva Santos <rafael.silva.sts@gmail.com>
2024-08-13 11:12:25 -0300

    Exemplo comentado dos operadores lógicos.

---
+++ python/capitulo-operadores-relacionais-e-operadores-logicos/teste-de-mesa-resultado.py
@@ -0,0 +1,14 @@
+a = 3
+b = 2
+c = 4
+resultado = (a + b) * c  < a / b and b ** c >= b - a
+
+#resultado = (3 + 2) * 4 < 3 / 2 and 2 ** 4 >= 2 - 3
+#resultado = (5) * 4 < 3 / 2 and 2 ** 4 >= 2 - 3
+#resultado = 5 * 4 < 3 / 2 and 2 ** 4 >= 2 - 3
+#resultado = 5 * 4 < 3 / 2 and 16 >= 2 - 3
+#resultado = 20 < 3 / 2 and 16 >= 2 - 3
+#resultado = 20 < 1,5 and 16 >= 2 - 3
+#resultado = 20 < 1,5 and 16 >= -1
+#resultado = False and True
+#resultado = False
